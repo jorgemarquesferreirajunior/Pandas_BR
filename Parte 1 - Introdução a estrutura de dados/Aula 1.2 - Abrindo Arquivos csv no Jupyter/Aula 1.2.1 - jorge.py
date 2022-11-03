3cd8aa2c99774aaef8324851986351d250fbf614
jorgemarquesferreirajunior <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2022-11-02 21:35:26 -0300

    primeira tentativa

--- Parte 1 - Introdução a estrutura de dados/Aula 1.2 - Abrindo Arquivos csv no Jupyter/Aula 1.2.1 - jorge.py
+++ Parte 1 - Introdução a estrutura de dados/Aula 1.2 - Abrindo Arquivos csv no Jupyter/Aula 1.2.1 - jorge.py
@@ -1,12 +1,37 @@
 import pandas as pd
 from tela_caminho import Ui_Form
-from PySide2 import QtWidgets
-path = "C:\Users\jorge\OneDrive\Área de Trabalho\Logimatec\LPS - estudo.csv"
-caminho = "r" + path
-print(caminho)
-# caminho = r"C:\Users\jorge\OneDrive\Área de Trabalho\Logimatec\LPS - estudo.csv"
-#
-#
-# df = pd.read_csv(caminho, encoding="UTF-8", sep=";")
-#
-# print(df)
+
+import sys
+from PySide2.QtWidgets import QApplication, QWidget, QMessageBox
+
+
+class Login(QWidget, Ui_Form):
+    def __init__(self) -> None:
+        super(Login, self).__init__()
+        self.path = None
+        self.setupUi(self)
+        self.setWindowTitle("Caminho")
+        self.btn_enviar_caminho.clicked.connect(self.copy_path)
+        self.btn_tabela.clicked.connect(self.generate_table)
+
+    def copy_path(self):
+        self.path = self.txt_caminho.text()
+        print(self.path)
+
+    def generate_table(self):
+        if self.path is not None:
+            df = pd.read_csv(self.path, encoding="UTF-8", sep=";")
+            print(df)
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle('Erro de Caminho')
+            msg.setIcon(QMessageBox.Information)
+            msg.setText('Primeiro insira um caminho na LineEdit')
+
+
+app = QApplication(sys.argv)
+
+window = Login()
+window.show()
+
+app.exec_()
--- Parte 1 - Introdução a estrutura de dados/Aula 1.2 - Abrindo Arquivos csv no Jupyter/tela_caminho.py
+++ Parte 1 - Introdução a estrutura de dados/Aula 1.2 - Abrindo Arquivos csv no Jupyter/tela_caminho.py
@@ -18,13 +18,25 @@ class Ui_Form(object):
         if not Form.objectName():
             Form.setObjectName(u"Form")
         Form.resize(541, 383)
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.txt_caminho = QLineEdit(Form)
         self.txt_caminho.setObjectName(u"txt_caminho")
-        self.txt_caminho.setGeometry(QRect(40, 50, 471, 31))
+        self.txt_caminho.setMinimumSize(QSize(470, 30))
         self.txt_caminho.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.txt_caminho)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout.addWidget(self.label)
+
         self.btn_enviar_caminho = QPushButton(Form)
         self.btn_enviar_caminho.setObjectName(u"btn_enviar_caminho")
-        self.btn_enviar_caminho.setGeometry(QRect(190, 160, 151, 51))
+        self.btn_enviar_caminho.setMinimumSize(QSize(150, 50))
         font = QFont()
         font.setPointSize(16)
         self.btn_enviar_caminho.setFont(font)
@@ -40,6 +52,50 @@ class Ui_Form(object):
 "}\n"
 "")
 
+        self.horizontalLayout.addWidget(self.btn_enviar_caminho)
+
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_3 = QLabel(Form)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout_2.addWidget(self.label_3)
+
+        self.btn_tabela = QPushButton(Form)
+        self.btn_tabela.setObjectName(u"btn_tabela")
+        self.btn_tabela.setMinimumSize(QSize(150, 50))
+        self.btn_tabela.setFont(font)
+        self.btn_tabela.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(0, 0, 0);\n"
+"	border-radius: 20px	\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"	background-color: rgb(255, 255, 255);\n"
+"	color: rgb(0, 0, 0);\n"
+"}\n"
+"")
+
+        self.horizontalLayout_2.addWidget(self.btn_tabela)
+
+        self.label_4 = QLabel(Form)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout_2.addWidget(self.label_4)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+
         self.retranslateUi(Form)
 
         QMetaObject.connectSlotsByName(Form)
@@ -48,6 +104,11 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.txt_caminho.setPlaceholderText(QCoreApplication.translate("Form", u"caminho", None))
+        self.label.setText("")
         self.btn_enviar_caminho.setText(QCoreApplication.translate("Form", u"Enviar", None))
+        self.label_2.setText("")
+        self.label_3.setText("")
+        self.btn_tabela.setText(QCoreApplication.translate("Form", u"TABELA", None))
+        self.label_4.setText("")
     # retranslateUi
 
