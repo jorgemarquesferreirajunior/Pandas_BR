adc6fed465c740ba01a12c3392758ad2d585ba9b
jorgemarquesferreirajunior <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2022-11-02 21:10:21 -0300

    criando tela_caminho

--- Parte 1 - Introdução a estrutura de dados/Aula 1.2 - Abrindo Arquivos csv no Jupyter/Aula 1.2.1 - jorge.py
+++ Parte 1 - Introdução a estrutura de dados/Aula 1.2 - Abrindo Arquivos csv no Jupyter/Aula 1.2.1 - jorge.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from tela_caminho import Ui_Form
+from PySide2 import QtWidgets
 path = "C:\Users\jorge\OneDrive\Área de Trabalho\Logimatec\LPS - estudo.csv"
 caminho = "r" + path
 print(caminho)
