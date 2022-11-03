218a3280afef7bb1fc567d758c5cf87b22a6acd1
jorgemarquesferreirajunior <104741179+jorgemarquesferreirajunior@users.noreply.github.com>
2022-11-02 21:53:45 -0300

    ìndices com pandas 01

--- Parte 1 - Introdução a estrutura de dados/Aula 1.3 - Series - Parte 1/Aula 1.3 - jorge.py
+++ Parte 1 - Introdução a estrutura de dados/Aula 1.3 - Series - Parte 1/Aula 1.3 - jorge.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 lista1 = [1, 4, 3, 6, 8, 9]
 
@@ -6,4 +8,7 @@ lista = [1, 4, "tesla", 3, 6.3, 8, 9]
 
 exemplo = pd.Series(lista)
 
-print(exemplo)
+# print(exemplo)
+
+print(pd.Series(np.random.randn(5), index=["a", "b", "c", "d", "e"]))   # Utilizando/Configurando Índices
+
